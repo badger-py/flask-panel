2bf1041acf7de4f9b54490078f62db8159d547f7
badger-py <yan090409@gmail.com>
2022-01-19 18:50:28 +0400

    Edit stukture

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template ,request
 
 
 app = Flask(__name__)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == "GET":
+        return render_template('login.html')
+    else:
+        
     return 'login'
 
 if __name__ == '__main__':
