74f15cf6518e2fe7caff4e9427bad0a16c16c74c
MrNektom <pvp.vdv@gmail.com>
2022-01-19 18:29:37 +0300

    Correct main.py

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ def login():
     if request.method == "GET":
         return render_template('login.html')
     else:
-        
-    return 'login'
+        return 'login'
 
 if __name__ == '__main__':
     app.run()
