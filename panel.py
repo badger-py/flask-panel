8c432d211cdec9ff863ce67488c8119dd4d6ba87
MrNektom <pvp.vdv@gmail.com>
2022-01-21 02:39:51 +0300

    Merging

--- panel.py
+++ panel.py
@@ -1,4 +1,5 @@
 import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class UsersController:
@@ -14,10 +15,11 @@ class UsersController:
         self.cursor.close()
         self.connection.close()
     # @staticmethod
-    # def use_db(func):
-    #     def wrapper(self):
+    # def use_db(func, **args):
+    #     def wrapper(self):#self):
+    #         print(self)
     #         self._get_database()
-    #         func()
+    #         func(self, *args)
     #         self._close_database()
 
     #     return wrapper
@@ -31,17 +33,24 @@ class UsersController:
     # @use_db
     def login_user(self, login, password):
         self._get_database()
-        self.cursor.execute('SELECT * FROM users WHERE username=? AND password_hash=?', (login, password))
+        self.cursor.execute('SELECT * FROM users WHERE username=?', (login,))
         user = self.cursor.fetchone()
         if not user:
+            self._close_database()
             return None
+        if not check_password_hash(user[-1], password):
+            self._close_database()
+            return None
+
         self._close_database()
         return User(*user)
     
     # @use_db
-    def create_user(self, role, username, password):
-        pass
-
+    def create_user(self, username, password):
+        self._get_database()
+        self.cursor.execute('INSERT INTO users(username, password_hash) VALUES(?, ?)', (username, generate_password_hash(password)))
+        self.connection.commit()
+        self._close_database()
     
     # @use_db
     def _create_databse(self):
@@ -76,3 +85,12 @@ class User:
         return str(self.id)
     def get_user(self):
         return str(self.id)
+
+class sql_connector:
+    def __init__(self, filename:str, connection:callable, execute_sql:callable, close_connectoin:callable, get_tables:callable):
+        self.filename = filename
+        self.connection = connection
+        self.execute_sql = execute_sql
+        self.close_connectoin = close_connectoin
+        self.get_tables = get_tables
+    
