b5754e8ca697938c44292e3854afc7da38cd0a3b
MrNektom <pvp.vdv@gmail.com>
2022-02-09 19:34:45 +0300

    Fix merging

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from unittest.mock import seal
 from flask import Flask, render_template ,request, redirect, url_for, flash, abort, make_response, jsonify
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
-from panel import *
+from users_controller import UsersController, User
+from data_views.sql import SQLTables
 from sqlite_db_connector import Connector, Table
+
 
 
 app = Flask(__name__)
